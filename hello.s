f1c476f392da5413fcbb6d765c6c9e81f8e2d0f1
Jon Craton <jncraton@gmail.com>
2026-01-15 10:41:05 -0500

    Replace tabs with spaces

--- hello.s
+++ hello.s
@@ -1,11 +1,11 @@
-.global	_start
+.global _start
 
 _start:
-	mov $1, %rax
-	mov $1, %rdi
-	mov $msg, %rsi
-	mov $14, %rdx
-	syscall
+  mov $1, %rax
+  mov $1, %rdi
+  mov $msg, %rsi
+  mov $14, %rdx
+  syscall
 
   mov $0x0a31, %r10
   mov $count, %rsi
@@ -13,23 +13,23 @@ _start:
   loop:
     mov %r10, (%rsi)
 
-  	mov $1, %rax
-  	mov $1, %rdi
-  	mov $count, %rsi
-  	mov $2, %rdx
-  	syscall
+    mov $1, %rax
+    mov $1, %rdi
+    mov $count, %rsi
+    mov $2, %rdx
+    syscall
 
     add $1, %r10
 
-  	cmp $0x0a34, %r10
-  	jl loop
+    cmp $0x0a34, %r10
+    jl loop
 
-	mov $60,%rax
-	mov $0,%rdi
-	syscall
+  mov $60,%rax
+  mov $0,%rdi
+  syscall
 
 msg:
-	.ascii "Hello, world!\n"
+  .ascii "Hello, world!\n"
 
 # Allocate loop counter
 .lcomm count 8
